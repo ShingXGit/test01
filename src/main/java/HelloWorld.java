03dacacd0d8c906c96f96be03b42ffe72b2541e3
situ <1689160731@qq.com>
2019-08-30 11:40:12 +0800

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * @author Administrator
+ * @date 2019/8/30
+ */
+
+/**
+ * @ClassName HelloWorld
+ * @Author: SiTu
+ * @Description: TODO
+ * @Date: Created in 11:36 2019/8/30
+ * @Version 1.0
+ */
+public class HelloWorld {
+}
